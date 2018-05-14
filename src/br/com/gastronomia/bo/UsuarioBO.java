d0f115d8d5a0623f918095847fd81fbd86fdf75f
Leonardo Marcelino Vieira <leonardo.marcelino@acad.pucrs.br>
2018-05-14 14:47:57 -0300

    FTP Cadastro

--- src/br/com/gastronomia/bo/UsuarioBO.java
+++ src/br/com/gastronomia/bo/UsuarioBO.java
@@ -4,7 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
+import br.com.gastronomia.util.TipoDeUsuario;
 import br.com.gastronomia.dao.UsuarioDAO;
 import br.com.gastronomia.dto.UsuarioLoginDTO;
 import br.com.gastronomia.exception.UsuarioInativoException;
@@ -95,6 +95,23 @@ public class UsuarioBO {
 		listUsers.put("Usuarios", usuarios);
 		return listUsers;
 	}
+
+	public HashMap<String, List<Usuario>> listProf() {
+		ArrayList<Usuario> usuarios = null;
+		HashMap<String, List<Usuario>> listUsers = new HashMap<String, List<Usuario>>();
+		usuarios = (ArrayList<Usuario>) usuarioDAO.listUsersByType(TipoDeUsuario.PROFESSOR);
+		listUsers.put("Usuarios", usuarios);
+		return listUsers;
+	}
+
+	public HashMap<String, List<Usuario>> listAlunos() {
+		ArrayList<Usuario> usuarios = null;
+		HashMap<String, List<Usuario>> listUsers = new HashMap<String, List<Usuario>>();
+		usuarios = (ArrayList<Usuario>) usuarioDAO.listUsersByType(TipoDeUsuario.USUARIO);
+		listUsers.put("Usuarios", usuarios);
+		return listUsers;
+	}
+
 
 	public Usuario getUserByCpf(Usuario usuarioLogin) throws ValidationException {
 		if (usuarioLogin != null) {
--- src/br/com/gastronomia/controllers/ReceitaController.java
+++ src/br/com/gastronomia/controllers/ReceitaController.java
@@ -14,6 +14,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.Date;
 
 @Path("receitas")
 public class ReceitaController {
@@ -37,6 +38,20 @@ public class ReceitaController {
         }
     }
 
+    @GET
+    @Path("/now")
+    @Produces("application/json; charset=UTF-8")
+    //@JWTTokenNeeded
+    public Response getDateTimeNow() throws PersistenciaException, SQLException {
+        try {
+            Date data = new Date();
+            return Response.ok().entity(data).status(Response.Status.ACCEPTED).build();
+
+        } catch (Exception e) {
+            return Response.ok().status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
     @POST
     @Path("/")
     @Consumes("application/json; charset=UTF-8")
@@ -45,6 +60,7 @@ public class ReceitaController {
     public Response create(Receita receita) throws PersistenciaException, ValidationException {
 
         try {
+            receita.setDatahora(new Date());
             System.out.println(receita);
             receitaBO.createReceita(receita);
         } catch (Exception e) {
@@ -75,19 +91,6 @@ public class ReceitaController {
 
         try {
             return Response.ok().entity(receitaBO.getReceitaById(id)).status(Response.Status.ACCEPTED).build();
-        } catch (Exception e) {
-            return Response.ok().status(Response.Status.BAD_REQUEST).build();
-        }
-
-    }
-
-    @GET
-    @Path("/usuario/{id}")
-    @Produces("application/json; charset=UTF-8")
-    //@JWTTokenNeeded
-    public Response searchByIDUsuario(@PathParam("id") Long id) throws PersistenciaException, ValidationException {
-        try {
-            return Response.ok().entity(receitaBO.getReceitaByIdUsuario(id)).status(Response.Status.ACCEPTED).build();
         } catch (Exception e) {
             return Response.ok().status(Response.Status.BAD_REQUEST).build();
         }
