51a5b98adcc9ad94e71b7e6444e2b044fb007606
Guga <gustavo.pinheiro.001@acadpucrs.br>
2018-05-30 17:45:26 -0300

    adicionando testes unitarios para validacao de cpf

--- test/br/com/gastronomia/tests/TestUsuarioBO.java
+++ test/br/com/gastronomia/tests/TestUsuarioBO.java
@@ -2,11 +2,19 @@ package br.com.gastronomia.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import br.com.gastronomia.bo.UsuarioBO;
+import br.com.gastronomia.model.Ingrediente;
+import br.com.gastronomia.model.Usuario;
 import org.junit.Test;
 
 import br.com.gastronomia.util.Validator;
+import br.com.gastronomia.util.Util;
 
 public class TestUsuarioBO {
+
+
+	// Testes do semestre anterior da classe Validator
+
 	@Test
 	public void testCPFTrue() {
 		assertEquals(true, Validator.validaCpf.fazConta(("61642096091")));
@@ -16,4 +24,22 @@ public class TestUsuarioBO {
 		assertEquals(false, Validator.validaCpf.fazConta(("85172596021")));
 	}
 
+	// Testes a partir da Sprint 3 - 2018
+	// Testes da Classe Validator
+	@Test
+	public void testCpfNaoAceitaLetras() {assertEquals(false, Validator.validaCpf.fazConta("abcdefghijk"));}
+
+	@Test
+	public void testCpfNaoAceitaMenosDeOnzeNumeros(){assertEquals(false, Validator.validaCpf.fazConta("12345678910"));}
+
+	@Test
+	public void testCpfNaoAceitaMaisDeOnzeNumeros(){assertEquals(false, Validator.validaCpf.fazConta("123456789011"));}
+
+	// Testes da Classe util
+
+	@Test
+	public void testNaoAceitaSequenciaDeNumerosIguais () {
+
+		assertEquals(false, Util.isCPF("1111111111"));
+	}
 }
